9eccec2ce1b2f079216a0d00f3219a589ffec0d5
Kayahs <akshaykmanchanda@gmail.com>
2018-10-24 15:32:48 -0400

    commit adding gulp and some styling changes

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+var gulp = require('gulp');
+
+var uglify = require('gulp-uglify'), 
+	rename = require('gulp-rename'), 
+	browserSync = require('browser-sync'),
+	eslint = require('gulp-eslint'),
+	clean = require('gulp-clean'),
+	sass = require('gulp-sass')
+	cleanCSS = require('gulp-clean-css');
+
+gulp.task('scripts', ['cleanJS'],/*['lint'],*/ function() {
+	return gulp.src('./scripts/*.js')
+	.pipe(uglify())
+	.pipe(rename({extname: '.min.js'}))
+	.pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('watch', ['browser-sync'], function() {
+	gulp.watch(['scripts/*.js', 'styles/sass/*.scss'], ['scripts', 'styles']);
+});
+
+gulp.task('browser-sync', function() {
+	browserSync.init(['scripts/*.js', 'styles/sass/*.scss'],{
+		server: {
+			baseDir: "./"
+		}
+	});
+});
+
+gulp.task('scsstocss', function() {
+	return gulp.src('./styles/sass/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./styles/css'));
+});
+
+gulp.task('styles', ['scsstocss', 'cleanCSS'], function() {
+	return gulp.src('./styles/css/*.css')
+	.pipe(cleanCSS())
+	.pipe(rename({extname: '.min.css'}))
+	.pipe(gulp.dest('./build/css'));
+})
+
+gulp.task('default',  ['styles', 'scripts'], function() {
+
+});
+
+
+/*gulp.task('lint', function() {
+	gulp.src('./scripts/*.js').pipe(eslint({
+		"extends": "eslint:recommended",
+		"rules": {
+			"no-alert": 1,
+			"camelcase": 1,
+			"curly": 1,
+			"eqeqeq": 1,
+			"no-console": 1,
+			"guard-for-in": 1,
+			"no-empty": 1,
+			"no-param-reassign": 1,
+			"no-unused-vars": 1,
+			"quotes": [ 1, "single"]
+		},
+		"globals": {
+		    "jQuery": false,
+		    "$": false
+		},
+		"env": {
+		"browser": true
+		}
+	}));
+});
+*/
+gulp.task('cleanAll', function() {
+	return gulp.src('build/**/*.*').pipe(clean());
+});
+
+gulp.task('cleanJS', function() {
+	return gulp.src('build/**/*.js').pipe(clean());
+});
+
+gulp.task('cleanCSS', function() {
+	return gulp.src('build/**/*.css').pipe(clean());
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,38 @@
 $(document).ready(function() {
 	$("#sectionSelector").change(function() {
+		var articleImg;
+		var articleImgCaption;
+		var articleExcerpt;
+		var articleLink;
+		var content;
+		var articleArea = $("#articleArea");
+		var header = $("header");
 		var url = "https://api.nytimes.com/svc/topstories/v2/"
 		var section = $("#sectionSelector option:selected").val() + ".json";
 		var apikey = "6a54040248ba4d669ff0dfc5db6bfd1b";
-		alert("Section selected is: " + section);
 		url += section;
 		url += '?' + $.param({
 			'api-key': apikey
 		});
-		$("#articleArea").append('<a href="' + url + '">' + url + '</a>');
+		articleArea.empty();
+		//articleArea.append('<a href="' + url + '">' + url + '</a>');
+		$.getJSON(url).done(function(data) {
+			console.log(data.results);
+			var count = 0;
+			articleArea.addClass('hasArticles');
+			header.addClass('hasArticles');
+			header.removeClass('noArticles');
+			$.each(data.results, function(key, value){
+				if (value.multimedia.length > 0) {
+					articleExcerpt = value.abstract;
+					articleImg = value.multimedia[3].url;
+					articleImgCaption = value.multimedia[3].caption;
+					articleLink = value.url;
+					content = '<a href="'+ articleLink + '" class="article"><img class="articleImg" src="' + articleImg + '" alt="'+ articleImgCaption + '"><span class="articleExcerpt">' + articleExcerpt + '</span></a>';
+					$("#articleArea").append(content);
+					return ++count < 12;
+				}
+			});
+		});
 	});
 });
